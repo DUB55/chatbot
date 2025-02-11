ea812078718f244ff6551a97ffcc2edc97ee718a
DUB55 <tcookdub5@gmail.com>
2025-02-11 15:21:38 +0100

    Update coder.py

--- dir/coder.py
+++ dir/coder.py
@@ -4,14 +4,11 @@ client = Client()
 
 def chatbot_response(user_input):
     response = client.chat.completions.create(
-    model="gpt-4",
-    messages=[{"role": "user", "content": user_input}],
-    web_search=False
+        model="gpt-4",
+        messages=[{"role": "user", "content": user_input}],
+        web_search=False
     )
-
     return response.choices[0].message.content
 
-
 if __name__ == "__main__":
-
     pass 
